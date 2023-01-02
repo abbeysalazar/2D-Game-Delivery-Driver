e127d8b2416677c1dab3b4c63b63b3081ac29013
Abbey Salazar <arxsalazar@gmail.com>
2023-01-01 23:51:15 -0600

    Add assets and camera manipulation to project

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -7,6 +7,10 @@ public class Collision : MonoBehaviour
     void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Ouch!");
-        
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        Debug.Log("What was that?!");   
     }
 }
