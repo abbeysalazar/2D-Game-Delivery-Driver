9d1f6fef3f7775111b0839d8c6c2e9aab1a21963
Abbey Salazar <arxsalazar@gmail.com>
2023-01-04 23:14:55 -0600

    Add additional comments

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    // fields for determining the color of the car based upon whether
+    // a package has been picked up or not
     [SerializeField] Color32 hasPackageColor = new Color32 (1, 1, 1, 1);
     [SerializeField] Color32 noPackageColor = new Color32 (1, 1, 1, 1);
 
+    // represents delay between package disappearing and driving over it
     [SerializeField] float destroyDelay = 0.5f;
 
     bool hasPackage = false;
@@ -20,11 +23,16 @@ public class Delivery : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D other)
     {
+        // text printed to console whenever the car collides with a
+        // non-package or customer object
         Debug.Log("Ouch!");
     }
 
     void OnTriggerEnter2D(Collider2D other) 
     {
+        // if the object is a package, print text to console to confirm,
+        // set hasPackage equal to true, change the car color, and
+        // destroy the object so that it disappears from the screen
         if (other.tag == "Package" && !hasPackage)
         {
             Debug.Log("Package collected");
@@ -35,7 +43,7 @@ public class Delivery : MonoBehaviour
         else if (other.tag == "Customer" && hasPackage)
         {
             Debug.Log("Package delivered");
-            hasPackage = false;
+            hasPackage = false; // set to false so a new package can be picked up
             spriteRenderer.color = noPackageColor;
         }
     }
--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -6,16 +6,16 @@ public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 1f;
     [SerializeField] float moveSpeed = 20f;
+
+    // fields representing movement speed after car drives over
+    // speed boost or bump
     [SerializeField] float slowSpeed = 15f;
     [SerializeField] float boostSpeed = 30f;
 
-    void Start()
-    {
-        
-    }
-
     void Update()
     {
+        // function is called once per frame
+        // allows car to be driven forward/backward and steered toward the left or right
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -steerAmount);
@@ -24,6 +24,8 @@ public class Driver : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
+        // when car drives over Boost-tagged object, moveSpeed increases,
+        // else when car drives over Bump-tagged object, moveSpeed decreases
         if (other.tag == "Boost")
         {
             moveSpeed = boostSpeed;
