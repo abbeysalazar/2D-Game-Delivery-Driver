442f916d353f4aa99bb87d7b247499a328e9cecc
Abbey Salazar <arxsalazar@gmail.com>
2023-01-04 21:10:32 -0600

    Add speed boosts and bumps and finalize game

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 1f;
-    [SerializeField] float moveSpeed = 0.01f;
+    [SerializeField] float moveSpeed = 20f;
+    [SerializeField] float slowSpeed = 15f;
+    [SerializeField] float boostSpeed = 30f;
 
     void Start()
     {
@@ -18,5 +20,17 @@ public class Driver : MonoBehaviour
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Boost")
+        {
+            moveSpeed = boostSpeed;
+        }
+        else if (other.tag == "Bump")
+        {
+            moveSpeed = slowSpeed;
+        }
     }
 }
